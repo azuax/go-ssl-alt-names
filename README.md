c9018f34a5d22c0bf849f19727b6af92a60c95d7
azuax <azuaxx@gmail.com>
2021-09-01 13:02:55 -0400

    First version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Search for alternate names on SSL certs
+
+## Usage
+```sh
+go run alt-names.go -f <filename> -t <threads>
+```
+### Filename with hosts
+- Must have the format `<host>:<port>
+- One per line
+
+## TODO
+- [ ] Save to a file
+- [ ] Colored output
---
+++ alt-names.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"sync"
+)
+
+type Params struct {
+	fn   string
+	nGor int
+}
+
+func getParams(args []string) Params {
+
+	if len(args) < 3 {
+		fmt.Printf("Usage %s -f <filename> -t <threads>\n", args[0])
+		os.Exit(1)
+	}
+
+	par := Params{}
+	flag.StringVar(&par.fn, "f", "", "File with hosts in format <host>:<port>. One per line")
+	flag.IntVar(&par.nGor, "t", 10, "Number of paralell goroutines")
+	flag.Parse()
+	return par
+}
+
+func getCertData(theChan chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	addr := <-theChan
+	conn, err := tls.Dial("tcp", addr, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	re := regexp.MustCompile(`^\*`)
+
+	for _, pc := range conn.ConnectionState().PeerCertificates {
+		for _, dns := range pc.DNSNames {
+			if !re.MatchString(dns) {
+				fmt.Printf("%s\t%s\n", addr, dns)
+			}
+		}
+	}
+}
+
+func main() {
+	PARAMS := getParams(os.Args)
+
+	WG := new(sync.WaitGroup)
+	gorChan := make(chan string, PARAMS.nGor)
+
+	f, err := os.Open(PARAMS.fn)
+	if err != nil {
+		panic("Can't open file")
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		gorChan <- scanner.Text()
+		WG.Add(1)
+		go getCertData(gorChan, WG)
+	}
+	WG.Wait()
+
+	fmt.Println("Done!")
+}
